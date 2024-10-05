a7c957277b0ea22786a30430d018b78ddf3cf2c6
babacar <nguiranebabacar305@gmail.com>
2024-10-05 02:40:22 +0000

    version 1

---
+++ src/app/acceuil/acceuil.component.ts
@@ -0,0 +1,13 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acceuil',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './acceuil.component.html',
+  styleUrl: './acceuil.component.css'
+})
+export class AcceuilComponent {
+
+}
