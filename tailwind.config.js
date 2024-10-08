e5817fabf6ec0477d6ab6f6efe4ba5116a38f1aa
babacar <nguiranebabacar305@gmail.com>
2024-10-08 23:16:40 +0000

    version 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { transform } = require('typescript');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,6 +10,41 @@ module.exports = {
       boxShadow: {
         ombre1 : '9px 15px 7.6px rgba(0, 0, 0, 0.35)',
         ombre2 : '0px 4px 32.7px rgba(0, 0, 0, 0.35)'
+      },
+      keyframes: {
+        fadeUp : {
+          "0%" : { transform : "translateY(30px) scale(0.9)", opacity : '0'},
+          "100%" : { transform : "translateY(0) scale(1)", opacity : '1'},
+        },
+        fadeIn : {
+          "0%" : { transform : "translateY(-30px) scale(0.9)", opacity : '0'},
+          "100%" : { transform : "translateY(0) scale(1)", opacity : '1'},
+        },
+        fadeRight : {
+          "0%" : { transform : "translateX(30px) scale(0.9)", opacity : '0'},
+          "100%" : { transform : "translateX(0) scale(1)", opacity : '1'},
+        },
+        fadeLeft : {
+          "0%" : { transform : "translateX(-30px) scale(0.9)", opacity : '0'},
+          "100%" : { transform : "translateX(0) scale(1)", opacity : '1'},
+        },
+        comeRight : {
+          "0%" : { transform : "translateX(50px)"},
+          "100%" : { transform : "translateX(0px)"}
+        },
+        comeLeft : {
+          "0%" : { transform : "translateX(-50px)"},
+          "100%" : { transform : "translateX(0px)"}
+        },
+      },
+      animation : {
+        fadeUp1 : "fadeUp 3s",
+        fadeUp2 : "fadeUp 5s",
+        fadeIn : "fadeIn 1s",
+        faderight : "fadeRight 3s",
+        fadeLeft : "fadeLeft 3s",
+        comeRight : "comeRight 2s",
+        comeLeft : "comeLeft 2s"
       }
     },
   },
